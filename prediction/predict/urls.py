b8a46e26c9335e429115fcc2a42b2cebc13282c3
Khoi Vo <nguyenkhoi8071@gmail.com>
2021-07-24 14:08:31 +0700

    Delete prediction directory

--- prediction/predict/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'predict'
-
-urlpatterns = [
-    path('', views.predict, name='predict'),
-    path('predict/', views.predict_chances, name= 'submit_prediction'),
-]
